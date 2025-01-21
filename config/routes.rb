c4cc43771687df0c739f14a03dad44bca1d10dbc
Martin Arning <158460540+mfa3000@users.noreply.github.com>
2025-01-21 15:56:54 +0100

    added navigation if not logged in

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "offers#index"
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,3 +14,4 @@ Rails.application.routes.draw do
 
   resources :offers, only: [:index, :new, :create]
 end
+
