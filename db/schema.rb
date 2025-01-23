12c9b77f5f10c5f4e21d83e288b5a9138d94860f
felixdelamare <delamare.felix@gmail.com>
2025-01-23 17:17:35 +0100

    Starting implementing location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_23_115549) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_23_140354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,9 +35,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_23_115549) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "category"
     t.float "latitude"
     t.float "longitude"
-    t.string "category"
     t.index ["user_id"], name: "index_cooking_classes_on_user_id"
   end
 
