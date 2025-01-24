f6886c41c01a50574c8ebdb8709ba2b06d818e5f
Chloe Lavat <lavat.chlo@gmail.com>
2025-01-24 10:52:13 +0100

    footer done

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_23_140354) do
-
+ActiveRecord::Schema[7.1].define(version: 2025_01_24_003024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,9 +63,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_23_140354) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.string "category"
     t.float "latitude"
     t.float "longitude"
+    t.string "category"
     t.index ["user_id"], name: "index_cooking_classes_on_user_id"
   end
 
